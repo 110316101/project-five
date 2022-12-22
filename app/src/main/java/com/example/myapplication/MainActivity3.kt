a1858a1f24ae0bcc763c76efa29278a762c45734
evelynlyn2003 <93966137+evelynlyn2003@users.noreply.github.com>
2022-12-22 22:23:08 +0800

    Update MainActivity3.kt

--- app/src/main/java/com/example/myapplication/MainActivity3.kt
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -7,27 +7,27 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
+import kotlin.math.roundToInt
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
-        val btn_bmi=findViewById<Button>(R.id.button2)
 
-    }
-    fun  bmi(view:View){
-        val ed_height=findViewById<EditText>(R.id.editTextTextPersonName)
-        val ed_weight=findViewById<EditText>(R.id.editTextTextPersonName2)
-        val weight=ed_weight.text.toString().toFloat()
-        val height=ed_height.text.toString().toFloat()
-        val bmi=weight/(height*height)
-        Log.d("BMI",bmi.toString()+weight.toString()+height.toString())
-        Intent(this,MainActivity3_1::class.java).apply {
-            putExtra("BMI_EXTRA",bmi)
-            putExtra("Weight",weight)
-            putExtra("Height",height)
-            startActivity(this)
+        val btn_send = findViewById<Button>(R.id.button2)
+
+        btn_send.setOnClickListener {
+            val ed_height = findViewById<EditText>(R.id.editTextTextPersonName).text.toString().toFloat()
+            val ed_weight = findViewById<EditText>(R.id.editTextTextPersonName2).text.toString().toFloat()
+            val bmi: Int = (((ed_weight/(ed_height*ed_height))*10000)*100).roundToInt()/100
+            Log.d("bmi", bmi.toString() )
+
+            val intent = Intent(this, MainActivity3_1::class.java)
+            intent.putExtra("height", ed_height.toString())
+            intent.putExtra("weight", ed_weight.toString())
+            intent.putExtra("bmi", bmi)
+            startActivity(intent)
         }
     }
-
 }
